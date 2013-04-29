468d8a410ac8f1d5743ae82df7e1662ae65054eb
Brandon Sandrowicz <brandon@sandrowicz.org>
2013-04-28 22:41:49 -0400

    Initial commit

---
+++ README.rst
@@ -0,0 +1,16 @@
+===================================
+StartupNorth.ca Jobs Postings Email
+===================================
+
+Scrape jobs.startupnorth.ca and create a nicely formatted text-based email
+notification.
+
+Credits
+-------
+
+Copyright 2013 Brandon Sandrowicz <brandon@sandrowicz.org>
+
+License
+-------
+
+MIT License. See LICENSE.
---
+++ setup.py
@@ -0,0 +1,27 @@
+from __future__ import print_function
+
+from glob import glob
+
+try:
+    from setuptools import setup
+except ImportError:
+    print("Falling back to distutils. Functionality may be limited.")
+    from distutils.core import setup
+
+requires = [ 'lxml', 'requests', 'cssselect' ]
+long_description = open('README.rst').read() + "\n\n" + open("Changelog").read()
+
+config = {
+    'name'            : 'startupnorth-jobs-notifiaction',
+    'description'     : 'Scrape jobs.startupnorth.ca and create a nice text-base email.',
+    'long_description': long_description,
+    'author'          : 'Brandon Sandrowicz',
+    'author_email'    : 'brandon@sandrowicz.org',
+    'url'             : 'https://github.com/bsandrow/startupnorth-jobs',
+    'version'         : '0.1',
+    'scripts'         : glob('bin/*'),
+    'install_requires': requires,
+    'license'         : open('LICENSE').read(),
+}
+
+setup(**config)
